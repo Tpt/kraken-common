7a87532de943595331a3f885c8f7b0a0fff6ca62
Niklas Rosenstein <rosensteinniklas@gmail.com>
2022-12-11 15:33:04 +0000

    add text module

--- src/kraken/common/__init__.py
+++ src/kraken/common/__init__.py
@@ -1,9 +1,15 @@
 from ._metadata import KrakenMetadata, KrakenMetadataException, metadata, metadata_capturing
+from ._text import inline_text, lazy_str, pluralize
 
 __all__ = [
+    # _metadata
     "metadata",
     "metadata_capturing",
     "KrakenMetadata",
     "KrakenMetadataException",
+    # _text
+    "pluralize",
+    "inline_text",
+    "lazy_str",
 ]
 __version__ = "0.1.0"
